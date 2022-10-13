5ab7003d13b549126c202a0b8586cc636698ed66
lil-armstrong <oebong1@gmail.com>
2022-10-13 21:56:18 +0000

    add task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - Main entry function
+  *
+  * Description: Main entry functio of the program
+  * Return: void
+  */
+int main(void)
+{
+	int i;
+	unsigned long int li;
+	unsigned long long int lli;
+	float f;
+	char c;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -13,3 +13,9 @@ The output should be saved in the file c
   - Use the function puts
   - You are not allowed to use printf
   - Your program should end with the value 0
+- [X] Write a C program that prints the size of various types on the computer it is compiled and run on.
+
+  - You should produce the exact same output as in the example
+  - Warnings are allowed
+  - Your program should return 0
+  - You might have to install the package libc6-dev-i386 on your Linux to test the -m32 gcc option
