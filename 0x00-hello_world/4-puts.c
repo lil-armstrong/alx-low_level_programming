a9f4bbfe1f9c1c0aa77e7c28ffa9db59a0dfa617
lil-armstrong <oebong1@gmail.com>
2022-10-13 07:52:40 -0700

    Add new task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+  * main - Main entry function
+  *
+  * Description: program entry function
+  * Return void
+  */
+void main(void)
+{
+	char str[50];
+
+	strcpy(str, '"Programming is like building a multilingual puzzle');
+	puts(str);
+}
