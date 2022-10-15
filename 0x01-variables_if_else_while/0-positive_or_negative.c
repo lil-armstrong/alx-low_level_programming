400302d161fa294e319ff87828e217bd76a11c92
lil-armstrong <oebong1@gmail.com>
2022-10-14 19:28:31 -0700

    correct betty error

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+i#include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
 
@@ -14,7 +14,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0)
+	if (n > 0)
 		printf("%d is positive.\n", n);
 	else
 		printf("%d is negative.\n", n)
