47a3dc72207c3761c10c0573a93df344fb8cf8c7
lil-armstrong <oebong1@gmail.com>
2022-10-14 21:59:18 -0700

    [task 5]: new

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * Return: void
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+		putchar(i);
+		i += 1;
+	}
+	putchar('\n');
+	return (0);
+}
