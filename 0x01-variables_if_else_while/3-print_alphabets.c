feaabc9ae55395864ab63af7cb48bfd96b1eff77
lil-armstrong <oebong1@gmail.com>
2022-10-14 21:50:41 -0700

    [task 3]: new

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * Return: void
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c += 1;
+	}
+	c = 'A';
+	while (c <= 'Z')
+	{
+        	putchar(c);
+                 c += 1;
+	}
+	putchar('\n');
+	return (0);
+}
