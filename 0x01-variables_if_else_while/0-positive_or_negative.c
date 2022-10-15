9906c835c8f8fe0fe9841255e3673249c6b791c2
lil-armstrong <oebong1@gmail.com>
2022-10-14 19:19:22 -0700

    add task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry function 
+ *
+ * Description: The main entry function
+ * Return: void
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n>0)
+		printf("%d is positive\r\n", n);
+	else
+		printf("%d is negative\r\n", n)
+	return (0);
+}
