b3a106ab96632daf3d2309d1e60531aa239cfc1f
lil-armstrong <oebong1@gmail.com>
2022-10-14 22:50:23 -0700

    [task 101]: testing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+
+#define RED "\e[0;31m"
 /**
  * main - main fn
  *
@@ -7,7 +11,11 @@
  */
 int main(void)
 {
-	fprintf(stdout, "and that piece of art is useful\" - Dora Korpar,
-	2015-10-19\n");
+	char str1[35] = "and that place of art is useful\"";
+	char str2[30] = "- Dora Korpar, 2015_10-19\n\0";
+	strcat(str1, str2);
+
+	fprintf(stderr, "%s", str1);
 	return (1);
 }
+
