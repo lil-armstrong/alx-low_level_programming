7e812b1abe78cb3ad8aeee6ce1f3124fb17e15dd
lil-armstrong <oebong1@gmail.com>
2022-10-16 21:50:52 -0700

    [task 101]: use write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,9 +10,9 @@
 int main(void)
 {
 	char str1[65] = "and that place of art is useful\" - Dora Korpar, 2015-10-19";
-
+	
 	str1[strlen(str1)] = '\n';
-	fprintf(stderr, "%s", str1);
+	write(2, str1, strlen(str1));
 	return (1);
 }
 
