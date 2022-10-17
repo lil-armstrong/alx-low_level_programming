31f2e398a098e79d19045985dbce050ef22b997f
lil-armstrong <oebong1@gmail.com>
2022-10-16 22:59:13 -0700

    [9]: new

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i++);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar(10);
+	return (0);
+}
