aaf1df728e382c6bb395d5c3613a83282644c0af
lil-armstrong <oebong1@gmail.com>
2022-10-17 22:00:43 -0700

    done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * Return: void
+ */
+int main(void)
+{
+	int i = 'a';
+
+	while (i < 'z')
+	{
+		putchar(i);
+		i += 1;
+	}
+	putchar(10);
+	return (0);
+}
