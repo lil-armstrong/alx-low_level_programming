94e869b0138f5d5a847847129dd1b4638cde95f2
lil-armstrong <oebong1@gmail.com>
2022-10-18 21:34:50 -0700

    [4 - 9]: new

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+	int n = 0, m = 0;
+	while (m <= 9)
+	{
+		n = 0;
+		while (n <= 9)
+		{
+			_putchar(m * n);
+			if (n < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			n++;
+		}
+		_putchar(10);
+		m++;
+	}
+}
