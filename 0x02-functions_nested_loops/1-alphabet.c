421d0049107e5a54cf99a59fec5274051017551a
lil-armstrong <oebong1@gmail.com>
2022-10-18 20:45:16 -0700

    [1]: new

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Main function
+ *
+ * Description: prints lowercase alphabets, followed by a newline
+ * Return: 0
+ */
+
+ void print_alphabet(void)
+ {
+	int c = 'a';
+
+	while(c <= 'z')
+	{
+		_putchar(c++);
+	}
+	_putchar(10);
+	return (0);
+ }
