dee2b5bca2ed0e2547583b640a2c9753e7b75711
lil-armstrong <oebong1@gmail.com>
2022-10-18 23:06:08 -0700

    [100]: new

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints n times table, starting with 0
+ * @b - time table to print
+ */
+void print_times_table(int p)
+{
+	int n = 0, m = 0, o;
+
+	while (m <= p && p < 15 && p >= 0)
+	{
+		n = 0;
+		while (n <= p)
+		{
+			o = m * n;
+			if (n > 0 && n <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (o > 9)
+			{
+				_putchar((o / 10) + '0');
+			}
+			else
+			{
+				if (n > 0)
+					_putchar(' ');
+			}
+			_putchar((o % 10) + '0');
+			n++;
+		}
+		_putchar(10);
+		m++;
+	}
+}
