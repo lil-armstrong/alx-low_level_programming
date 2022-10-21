2a854dc1fd17cf4652629535fb795edcefcff529
lil-armstrong <oebong1@gmail.com>
2022-10-20 21:57:27 -0700

    [2]: new

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: arg 1
+ * @b: arg 2
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 int _isupper(int c);
 int _isdigit(int c);
 void print_numbers(void);
+int mul(int a, int b);
