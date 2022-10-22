57415f99c09964f618e5a060502fb8dc175502b7
lil-armstrong <oebong1@gmail.com>
2022-10-21 17:56:02 -0700

    [9]: new

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * isMultiple - Checks whether n is a multiple of m
+ * @n: the integer to check
+ * @m: thr multiplier
+ *
+ * Return: integer
+ */
+int isMultiple(int n, int m)
+{
+	return (n % m == 0);
+}
+
+/**
+ * main - Program main function
+ *
+ * Return: Always(0)
+ */
+int main(void)
+{
+	int count = 1, limit = 100, m3, m5;
+
+	while (count <= limit)
+	{
+		m3 = isMultiple(count, 3);
+		m5 = isMultiple(count, 5);
+
+		if (m3 && m5)
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		else if (m3)
+		{
+			printf("%s ", "Fizz");
+		}
+		else if (m5)
+		{
+			printf("%s ", "Buzz");
+		}
+		else
+			printf("%d ", count);
+		count++;
+	}
+	printf("\n");
+
+	return (0);
+}
