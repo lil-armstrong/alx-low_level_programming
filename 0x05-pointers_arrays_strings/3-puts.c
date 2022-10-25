b2416ad7b6237b0df1a701f6b388847314cd4d8f
lil-armstrong <oebong1@gmail.com>
2022-10-25 15:17:52 -0700

    [3]: new

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_puts - prints a string, followed by a new line, to stdout.
+ * @str: array pointer of type char
+ *
+ * Return: int
+ */
+
+void _puts(char *str)
+{
+	int n = 0;
+
+	while (*(s + n))
+	{
+		_putchar(*(s + n));
+		n++;
+	}
+	_putchar(10);
+	return (n);
+}
+
