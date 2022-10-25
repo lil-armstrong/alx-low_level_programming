3e45503f1541108f8837e0d81a4f70f3dbe6a36e
lil-armstrong <oebong1@gmail.com>
2022-10-25 16:20:58 -0700

    [4]: new

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, followed by a new line, to stdout.
+ * @s: array pointer of type char
+ *
+ */
+
+void print_rev(char *s)
+{
+	int n = 0;
+
+	while (*(str + n)
+	{
+		n++;
+	}
+
+	while (n)
+	{
+		_putchar(str[n]);
+		--n
+	}
+	_putchar(10);
+}
+
