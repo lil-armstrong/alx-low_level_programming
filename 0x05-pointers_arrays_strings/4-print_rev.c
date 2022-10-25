50f036833b6f60af2df6a43bc74da8e6986fad62
lil-armstrong <oebong1@gmail.com>
2022-10-25 16:22:56 -0700

    [fix 4]: missing variable error

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,11 +2,11 @@
 
 /**
  * print_rev - prints a string, followed by a new line, to stdout.
- * @s: array pointer of type char
+ * @str: array pointer of type char
  *
  */
 
-void print_rev(char *s)
+void print_rev(char *str)
 {
 	int n = 0;
 
