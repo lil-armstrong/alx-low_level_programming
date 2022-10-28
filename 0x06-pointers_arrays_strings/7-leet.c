54554dde1eff498816562d8e97d82abc94640a13
lil-armstrong <oebong1@gmail.com>
2022-10-28 16:15:53 -0700

    [7]: new

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,10 +1,28 @@
-#include "main..h"
+#include "main.h"
 
 /**
  *
  */
 char *leet(char *s)
 {
+	char m[10] = {
+		'4', 'a',
+		'3', 'e',
+		'0', 'o',
+		'7', 't',
+		'1', 'l'
+	};
+	int i = 0, j;
 
+	while (s[i])
+	{
+		for (j = 1; j < 5; j += 2)
+		{
+			if (s[i] == m[j] || s[i] == (char)(m[j] - 32))
+				s[i] = m[j - 1];
+		}
+		++i;
+	}
 	return (s);
 }
+
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -34,11 +34,13 @@ void print_array(int *a, int n)
  */
 int main(void)
 {
-char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-    char *ptr;
 
-    ptr = cap_string(str);
-    printf("%s", ptr);
-    printf("%s", str);
+	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+    char *p;
+
+    p = leet(s);
+    printf("%s", p);
+    printf("%s", s);
     return (0);
+	return (0);
 }
