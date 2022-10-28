5b74fbbbc3a69423bc24fb80d9b8cb41d1d771fd
lil-armstrong <oebong1@gmail.com>
2022-10-28 16:32:58 -0700

    [7]: fix logic error

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,21 +8,25 @@
  */
 char *leet(char *s)
 {
-	char m[10] = {
+	char m[] = {
 		'4', 'a',
 		'3', 'e',
 		'0', 'o',
 		'7', 't',
 		'1', 'l'
 	};
-	int i = 0, j;
+	int i = 0, j, diff = (int)('a' - 'A');
 
 	while (s[i])
 	{
-		for (j = 1; j < 5; j += 2)
+		for (j = 1; m[j]; j += 2)
 		{
-			if (s[i] == m[j] || s[i] == (char)(m[j] - 32))
+			if (s[i] == m[j] || s[i] == (char)(m[j] - diff))
+			{
+				m[j]);
 				s[i] = m[j - 1];
+				break;
+			}
 		}
 		++i;
 	}
