0876e0f10e0ff65c473ffc4b5b8154356bc67fdd
lil-armstrong <oebong1@gmail.com>
2022-10-28 12:07:32 -0700

    [3]:new

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ void reverse_array(int *a, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
 
 #endif
