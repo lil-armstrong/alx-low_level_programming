028d105b1d97f15d8a4fa97a92a1a27ff88f7ba9
lil-armstrong <oebong1@gmail.com>
2022-11-01 10:00:57 -0700

    fix lint error

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _memset - fills a memeory area, s with a character, b,
+ * *_memset - fills a memeory area, s with a character, b,
  * n nunber of times
- * @ s - memory area pointer
- * @b - character to fill memory area with
- * @n - number of memory area to fill from the start
+ * @s: memory area pointer
+ * @b: character to fill memory area with
+ * @n: number of memory area to fill from the start
  *
  * Return: pointer to start of memory area
  */
-char *_memset(char*s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	while (n)
 	{
 		*(s + --n) = b;
 	}
-	return s;
+	return (s);
 }
