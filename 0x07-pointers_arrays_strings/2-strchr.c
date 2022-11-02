fa5feb57fb06f2b8d4f2e0de21e8e4088da486d0
lil-armstrong <oebong1@gmail.com>
2022-11-02 05:52:14 -0700

    [2]

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,18 +8,11 @@
  */
 char *_strchr(char *s, char c)
 {
-		int a;
-
-		while (1)
-		{
-			a = *s++;
-			if (a == c)
-			{
-				return (s - 1);
-			}
-			if (a == 0)
-			{
-				return (NULL);
-			}
-		}
+	while (*s)
+	{
+		if (*s ==  c)
+			return (s--);
+		s++;
+	}
+	return (NULL);
 }
