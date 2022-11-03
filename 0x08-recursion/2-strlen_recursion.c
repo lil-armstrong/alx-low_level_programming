71946f8307ad304fd41ef9a525a15ac223e1927f
lil-armstrong <oebong1@gmail.com>
2022-11-02 22:52:31 -0700

    [2]: new

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * charLoop - Loops thru a character string recursively
+ * @s: character string
+ * @index: tracking index
+ *
+ * Return: total length of the string
+ */
+int charLoop (char *s, int index)
+{
+	if (*s)
+	{
+		++index;
+		return charLoop((s + 1), index);
+	}
+	return index;
+}
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: pointer to array of characters to print
+ * Return: Length of string
+ */
+int _strlen_recursion(char *s)
+{
+	return (charLoop(s, 0));
+}
