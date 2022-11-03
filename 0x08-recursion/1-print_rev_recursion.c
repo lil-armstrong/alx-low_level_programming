71b78d040feb129b20d4d3efc5e4ba1681e8f501
lil-armstrong <oebong1@gmail.com>
2022-11-02 21:57:55 -0700

    [1]: new

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: pointer to array of characters to print
+ */
+void _print_rev_recursion(char *s)
+{
+	if (!*s)
+		putchar('\0');
+	else
+	{
+		_print_rev_recursion((s + 1));
+		putchar(*s);
+	}
+}
