94dde4c21aeabe721a2da446619defadcf17f00e
lil-armstrong <oebong1@gmail.com>
2022-11-09 21:43:37 -0800

    [4]: new

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include  <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Main function
+ *
+ * @argc: number of command line arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 always if successful
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	int result, arg;
+
+	result = 0;
+
+	while (*(++argv))
+	{
+		arg = atoi(*argv);
+		if (arg)
+		{
+			result += arg;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
