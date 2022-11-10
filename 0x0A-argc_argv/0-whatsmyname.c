324b30cc56982cbb9d7135ecf381e6858b40157e
lil-armstrong <oebong1@gmail.com>
2022-11-09 21:00:17 -0800

    fix lint error

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include  <stdio.h>
-
+/**
+ * main - Main function
+ *
+ * @argc: number of command line arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 always if successful
+ */
 int main(int argc, char **argv)
 {
 	printf("%s\n", argv[0]);
