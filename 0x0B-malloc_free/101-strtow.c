0d9ef8105a8ce4e19a5a095d57f2403ca87e6053
lil-armstrong <oebong1@gmail.com>
2022-11-12 05:11:33 -0800

    add adv task 2

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * strtow - splits string to words
+ * @str: pointer to string
+ * Return: NULL(fail), char*(success)
+ */
+char **strtow(char *str)
+{
+	int row = 0, col = 0, len = 0, tlen = 0, j;
+	char **pt;
+
+	if (!str || str == NULL)
+		return (NULL);
+	while (str[len] != '\0')
+		len++
+	pt = (char **) malloc(sizeof(char *) * len + 1);
+	while (col < len)
+	{
+		tlen = 0;
+		while (str[col + tlen] != ' ')
+			tlen++;
+		if (tlen)
+		{
+			pt[row] = malloc(sizeof(char) * tlen);
+			if (pt[row] == NULL)
+			{
+				j = col;
+				while (--j)
+				{
+					free(pt[j]);
+				}
+				free(pt);
+			}
+			j = 0;
+			while (tlen--)
+				pt[row][j++] = str[col++];
+			++row;
+		}
+		else
+			col++;
+	}
+	pt[row] = NULL;
+	return (pt);
+}
