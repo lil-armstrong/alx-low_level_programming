c9a3c2a274ad65660d854c0051310a4744a785a1
lil-armstrong <oebong1@gmail.com>
2022-11-12 07:06:40 -0800

    fix lint errors

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,7 +1,39 @@
+
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * length_with_word_count - Counts a string character and words
+ * separated by a specified separator character
+ * @str: string pointer
+ * @sep: separator character
+ * Return: [length_of_string, word_count]
+ */
+int *length_with_word_count(char *str, char sep)
+{
+	int w = 0, len = 0, flag = 0, arr[2];
+	int *result = arr;
+
+	while (str[len] != '\0')
+	{
+		if (str[len] != sep)
+		{
+			flag = 0;
+		}
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+		len++;
+	}
+
+	result[0] = len;
+	result[1] = w;
+
+	return (result);
+}
 /**
  * strtow - splits string to words
  * @str: pointer to string
@@ -9,27 +41,27 @@
  */
 char **strtow(char *str)
 {
-	int row = 0, col = 0, len = 0, tlen = 0, j, rlen = 0;
+	int row = 0, col = 0, len = 0, word_count = 0, j, words = 0, *count;
 	char **pt;
 
 	if (str == NULL)
 		return (NULL);
-	while (str[rlen] != '\0')
+
+	count = length_with_word_count(str, ' ');
+	len = count[0];
+	words = count[1];
+
+	pt = (char **) malloc(sizeof(char *) * (words));
+	printf("Word count: %d\n", words);
+	while (pt != NULL && len && col < len)
 	{
-		if (str[rlen] != ' ')
-			len++;
-		rlen++;
-	}
-	pt = (char **) malloc(sizeof(char *) * len );
-	while (len && col < rlen)
-	{
-		tlen = 0;
-		while (str[col + tlen] != ' ')
-			tlen++;
-		if (tlen)
+		word_count = 0;
+		while (str[col + word_count] != ' ')
+			word_count++;
+
+		if (word_count)
 		{
-			total_chars += tlen;
-			pt[row] = malloc(sizeof(char) * tlen);
+			pt[row] = malloc(sizeof(char) * word_count);
 			if (pt[row] == NULL)
 			{
 				j = col;
@@ -40,7 +72,7 @@ char **strtow(char *str)
 				free(pt);
 			}
 			j = 0;
-			while (tlen--)
+			while (word_count--)
 				pt[row][j++] = str[col++];
 			++row;
 		}
