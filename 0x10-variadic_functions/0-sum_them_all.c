f5eb66966cbe8335562a359e3eda57aa553b000e
lil-armstrong <oebong1@gmail.com>
2022-11-18 11:34:20 -0800

    feat:add task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,18 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list al;
+	unsigned int i, sum = 0;
+
+	if (n == 0)
+		return 0;
+	va_start(al, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(al, int);;
+	}
+	va_end(al);
+	return sum;
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,4 @@
+#ifndef VA_FN
+#define VA_FN
+int sum_them_all(const unsigned int n, ...);
+#endif
