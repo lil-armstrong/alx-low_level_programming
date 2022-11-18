0f6cbdd5289d5063d0107b99f37b94bb3426991d
lil-armstrong <oebong1@gmail.com>
2022-11-18 11:36:15 -0800

    fix lint error

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,12 +7,12 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i, sum = 0;
 
 	if (n == 0)
-		return 0;
+		return (0);
 	va_start(al, n);
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(al, int);;
+		sum += va_arg(al, int);
 	}
 	va_end(al);
-	return sum;
+	return (sum);
 }
