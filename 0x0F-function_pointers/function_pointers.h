3c63069a34e97d2e8979d549f7869cdee0985634
lil-armstrong <oebong1@gmail.com>
2022-11-19 07:29:59 -0800

    add stdlib to header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,8 @@
+#include <stdlib>
+#include <stdio.h>
 #ifndef FUNC_PTR_H
 #define  FUNC_PTR_H
 
-#include <stdio.h>
-#include <stdlib>
 int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
 void  array_iterator(int *array, size_t size, void (*action)(int));
