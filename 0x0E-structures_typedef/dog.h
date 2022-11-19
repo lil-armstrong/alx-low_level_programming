d7673dc1aa4659ebb35e0100186ab4f52a3ff8c1
lil-armstrong <oebong1@gmail.com>
2022-11-19 10:15:28 -0800

    feat: add dog header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: name of the dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
