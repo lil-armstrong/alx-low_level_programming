bd26efe7caf4780e6f5d66117463ec6296fbc800
lil-armstrong <oebong1@gmail.com>
2022-11-19 10:34:37 -0800

    fix betty lint

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,13 @@
 #include "dog.h"
 
+/**
+ * init_dog - initializes a variable of type dog
+ * @d: strcuuct of dog
+ * @name: name field
+ * @age: age field
+ * @owner: owner field
+ * Return: nothing
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
