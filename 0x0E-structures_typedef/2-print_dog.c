253b3bc6157302c1cf1b9a3e9b050a8801242957
lil-armstrong <oebong1@gmail.com>
2022-11-19 23:31:05 -0800

    fix logic

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,16 +10,13 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
-	{
-		if (d->name == NULL)
-			d->name = "(nil)";
-		if (d->owner == NULL)
-			d->owner = "(nil)";
-		if (d->age == NULL)
-			d->age = "(nil)";
+	if (d == NULL)
+		return;
 
-		printf("Name: %s\nAge: %f\nOwner: %s\n",
-		       d->name, d->age, d->owner);
-	}
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
