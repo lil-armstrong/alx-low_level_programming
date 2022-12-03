282ee0919ecd0d437a8b147ca811b0659946c54a
lil-armstrong <oebong1@gmail.com>
2022-12-03 07:47:43 -0800

    fix betty doc

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,11 @@
 #include "lists.h"
 
 /**
- * listint_len - he beginning of a linked list
- * @head: pointer to the first node in the list
- * @n: data to insert in that new node
+ * listint_len - returns the number of elemnts in a linked list
+ * @h: pointer to the first node in the list
  *
- * Return: pointer to the new node, or NULL if it fails
- */
+ * Return: number of element in list
+  */
 size_t listint_len(const listint_t *h)
 {
 	const listint_t *pt;
