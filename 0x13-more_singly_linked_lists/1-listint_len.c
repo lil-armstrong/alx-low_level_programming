b59e915cc586a37fadf9d7e89cad01e42855ee7d
lil-armstrong <oebong1@gmail.com>
2022-12-03 07:44:29 -0800

    add task 3

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,23 +1,24 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node at the beginning of a linked list
+ * listint_len - he beginning of a linked list
  * @head: pointer to the first node in the list
  * @n: data to insert in that new node
  *
  * Return: pointer to the new node, or NULL if it fails
  */
-listint_t *add_nodeint(listint_t **head, const int n)
+size_t listint_len(const listint_t *h)
 {
-	listint_t *new;
+	const listint_t *pt;
+	size_t len;
 
-	new = malloc(sizeof(listint_t));
-	if (!new)
-		return (NULL);
+	len = 0;
+	pt = h;
 
-	new->n = n;
-	new->next = *head;
-	*head = new;
-
-	return (new);
+	while (pt != NULL)
+	{
+		len++;
+		pt = pt->next;
+	}
+	return (len);
 }
