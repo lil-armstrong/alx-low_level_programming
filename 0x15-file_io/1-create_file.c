08af9ef2146c98de9993089229dc4970d23c852f
lil-armstrong <oebong1@gmail.com>
2022-12-06 05:30:22 -0800

    fix error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,24 +9,38 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, r, w;
 	char *err;
 
 	if (filename != NULL)
 	{
-		fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-
-		if (fd == -1)
-		{
-			err = "File cannot be created, written to or truncated";
-			write(STDERR_FILENO, err, strlen(err));
-			return (-1);
-		}
+		fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 
 		if (text_content != NULL)
-			write(fd, text_content, strlen(text_content));
+			w = write(fd, text_content, _strlen(text_content));
+
+		if (fd == -1 || r == -1 || w == -1)
+			return (-1);
+
 		close(fd);
 		return (1);
 	}
 	return (-1);
+}
+
+/**
+ * _strlen - gets string length
+ * @str: NULL terminated character string
+ * Return: int
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
 }
