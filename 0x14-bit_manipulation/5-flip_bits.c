793a317f291146b0fdfed26b46617353ac071e6b
lil-armstrong <oebong1@gmail.com>
2022-12-08 23:42:37 -0800

    fix betty lint error

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,6 +1,7 @@
-
+#include "main.h"
 /**
- * flip_bits - returns the number of bits you would need to flip to get from one number to another.
+ * flip_bits - returns the number of bits you would need to
+ * flip to get from one number to another.
  * @n: first number
  * @m: second number
  * Return: number of flips (success)
@@ -12,7 +13,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	while (bits > 0)
 	{
 		++count;
-		bits &= (bits - 1);
+		bits &= (bits--);
 	}
 	return (count);
 }
