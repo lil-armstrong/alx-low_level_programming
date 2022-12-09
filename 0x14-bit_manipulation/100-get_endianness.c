0816ac4267005097f7623b0bd50a2bd6c5bfacfa
lil-armstrong <oebong1@gmail.com>
2022-12-09 00:56:05 -0800

    code refactor

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,18 +1,15 @@
 /*100-get_endianness.c*/
 #include "main.h"
-/*
-	Big endian aka network byte order
-	Little endian akahost byte order
-*/
-
+/**
+ * get_endianness - check the endianness
+ * Return: 0 (Big endian), 1 (Little endian)
+ */
 int get_endianness(void)
 {
 	unsigned int x = 0x76543210;
-	char *c = (char*) &x;
+	char *c = (char *) &x;
 
 	if (*c == 0x10)
-		/*Little endianness*/
 		return (1);
-	/*Big endianness*/
 	return (0);
 }
