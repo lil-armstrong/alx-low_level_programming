eb7cd43c07c6fc7f21e57a489823aba2ca64bc82
lil-armstrong <oebong1@gmail.com>
2022-12-09 00:59:42 -0800

    code refactor

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -14,5 +14,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 void _putchar(char c);
-
+int get_endianness(void);
 #endif
