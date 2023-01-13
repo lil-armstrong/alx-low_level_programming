97e5fdb43a4666c29f6d407a2efde48d0410a680
lil-armstrong <oebong1@gmail.com>
2023-01-13 04:20:38 +0100

    feat 0x17(1)

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - return the number of elements in a linked dlistint_t list
+ * @h: list head
+ * Return: int
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int len = 0;
+	const dlistint_t *current;
+
+	if (h != NULL)
+	{
+		if (h->prev == NULL)
+		{
+			current = h;
+
+			/* clang-format off */
+			do {
+				++len;
+			} while(current != NULL);
+		}
+	}
+	return (len);
+}
