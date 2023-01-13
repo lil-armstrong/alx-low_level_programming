f8b7e19dcf70cfe052ddd4e4ef5e1ec483af0123
lil-armstrong <oebong1@gmail.com>
2023-01-13 06:22:54 +0100

    refactor 0x17(5)

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -8,47 +8,21 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t *node = NULL;
-	const unsigned int len = dlistint_len(head);
+	unsigned int len = 0;
 
 	if (head != NULL)
 	{
-		if (index < len - 1)
+		while (head->prev != NULL)
+			head = head->prev;
+
+		while (head != NULL)
 		{
-			node = head;
-
-			while (index && node != NULL)
-			{
-				node = node->next;
-				--index;
-			}
-		}
-	}
-	return (node);
-}
-
-/**
- * dlistint_len - return the number of elements in a linked dlistint_t list
- * @h: list head
- * Return: int
- */
-
-size_t dlistint_len(const dlistint_t *h)
-{
-	int len = 0;
-	const dlistint_t *current;
-
-	if (h != NULL)
-	{
-		while (h->prev != NULL)
-			h = h->prev;
-
-		current = h;
-		while (current != NULL)
-		{
+			if (len == index)
+				break;
+			head = head->next;
 			++len;
-			current = current->next;
 		}
 	}
-	return (len);
+
+	return (head);
 }
