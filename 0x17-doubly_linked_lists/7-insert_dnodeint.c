6a607b02341b2269e0a31ff290d8951ea929abec
lil-armstrong <oebong1@gmail.com>
2023-01-13 06:52:12 +0100

    fix 0x17(7): linting

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -41,7 +41,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int index, int n)
 		}
 
 		if (len++ == index)
-			return add_dnodeint_end(h, n);
+			return (add_dnodeint_end(h, n));
 	}
 
 	return (node);
