691c9b7cd14d8ade5a4d22840678a874461755e6
lil-armstrong <oebong1@gmail.com>
2023-01-13 04:58:45 +0100

    patch 0x17(2): refactor logic

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,25 +8,31 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *node = NULL, *current;
+	dlistint_t *node;
+	dlistint_t *current;
 
 	if (head != NULL)
 	{
 		current = *head;
-		if (current->prev == NULL)
-		{
-			/* Create a new node */
-			node = malloc(sizeof(dlistint_t));
 
-			if (node != NULL)
-			{
-				/* Set prev to null */
-				node->prev = NULL;
-				/* Set next to point to current head */
-				node->next = current;
-				/* Point the current head prev to the new node */
-				*head = node;
-			}
+		/* Ensure we are at the head of the list */
+		while (current->prev != NULL)
+			current = current->prev;
+
+		/* Create a new node */
+		node = malloc(sizeof(dlistint_t));
+
+		if (node != NULL)
+		{
+			/* Set node n value to n */
+			node->n = n;
+			/* Set prev to null */
+			node->prev = NULL;
+			/* Set next to point to current head */
+			node->next = current;
+
+			/* Point the current head prev to the new node */
+			current->prev = node;
 		}
 	}
 
