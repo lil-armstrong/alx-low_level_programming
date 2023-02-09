1d0bac0f54eaabc3119c4d48363a898ab282c748
lil-armstrong <oebong1@gmail.com>
2023-02-09 05:50:18 +0100

    refactor 0 (0x1A)

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,13 +8,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht = malloc(sizeof(hash_table_t) * size);
+	/* int i = 0; */
 
 	if (ht != NULL)
 	{
 		ht->size = size;
 		ht->array = malloc(sizeof(hash_node_t *) * size);
-
-		return (ht);
+		if (ht->array != NULL)
+		{
+			/* for (i = 0; i < size; i++)
+				ht->array[i] = NULL; */
+			return (ht);
+		}
 	}
 	return (NULL);
 }
