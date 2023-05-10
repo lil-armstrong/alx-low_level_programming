b44fd81f96b7981e3cd1c5316b2f74f80739b45e
lil-armstrong <oebong1@gmail.com>
2023-05-10 03:46:48 +0100

    refactor

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,34 +1,26 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of integers using
- *                 the linear search algorithm
- * @array: pointer to the first element of the array to search in
- * @size: number of elements in the array
- * @value: value to search for
+ * linear_search - searches for a value in an array of
+ * integers using the Linear search algorithm
  *
- * Return: the index of the value in the array, or -1 if not found
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: Always EXIT_SUCCESS
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t index = 0;
+	int i;
 
 	if (array == NULL)
 		return (-1);
 
-	/* Ádd sentinel value*/
-	array[size] = value;
-
-	while (array[index] != value)
+	for (i = 0; i < (int)size; i++)
 	{
-		printf("Value checked array[%zu] = [%d]\n", index, array[index]);
-		index++;
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
+		if (value == array[i])
+			return (i);
 	}
-	/* Remove sentinel value */
-	array[size] = '\0';
-
-	if (index < size)
-		return (index);
-
 	return (-1);
 }
